769b047fef51f2f83c611885b8542d743b04ab0d
Iann <iconian4@gmail.com>
2025-03-19 22:32:10 +0300

    initialize commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraphElement = document.getElementById('text');
+
+    if (paragraphElement){
+        paragraphElement.textContent = 'This is really cool!';
+    }
+})
